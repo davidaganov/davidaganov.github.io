7bb824c8958ee21b2eea4049dcbd26b67aa96d53
David <davidaganov21@gmail.com>
2026-02-16 14:12:34 +0200

    fix routing

--- layers/base/app/components/app/AppRightSidebar.vue
+++ layers/base/app/components/app/AppRightSidebar.vue
@@ -8,12 +8,14 @@ defineProps<{
 </script>
 
 <template>
-  <Teleport to="#app-right-sidebar-root">
-    <aside class="sticky top-12 h-fit self-start">
-      <div class="space-y-6">
-        <AppRightSidebarToc :page="page" />
-        <AppRightSidebarMeta :page="page" />
-      </div>
-    </aside>
-  </Teleport>
+  <ClientOnly>
+    <Teleport to="#app-right-sidebar-root">
+      <aside class="sticky top-12 h-fit self-start">
+        <div class="space-y-6">
+          <AppRightSidebarToc :page="page" />
+          <AppRightSidebarMeta :page="page" />
+        </div>
+      </aside>
+    </Teleport>
+  </ClientOnly>
 </template>
